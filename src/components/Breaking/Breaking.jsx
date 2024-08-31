2df1e554205f7f471e729d82a9e0018f42f5ce1e
princemahmood117 <princemahmood117@gmail.com>
2024-08-31 17:01:43 +0600

    login register added

--- src/components/Breaking/Breaking.jsx
+++ src/components/Breaking/Breaking.jsx
@@ -1,15 +1,18 @@
 import Marquee from "react-fast-marquee";
 
 const Breaking = () => {
-    return (
-        <div className="flex" >
-            <button className="bg-slate-700 p-2 rounded-lg text-white ml-4">Latest</button>
-            <Marquee speed={100} className="text-red-500 ml-2">
-             <a className="ml-12" href="https://www.youtube.com/watch?v=HnaUjQ7zCzs">Click to play Far cry 5 theme song!</a>   
+  return (
+    <div>
 
-            </Marquee>
-        </div>
-    );
+      <Marquee speed={100} className="text-sky-600 ml-2 font-bold">
+        <p className="italic">
+       
+          Find Your Dream Property Today - Explore the Best Estates for Sale and
+          Rent!
+        </p>
+      </Marquee>
+    </div>
+  );
 };
 
 export default Breaking;
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -6,29 +6,40 @@ const Register = () => {
         <div className="heromin-h-screen">
         <div className="hero-content flex-col lg:flex-row-reverse">
           <div className="text-center lg:text-left">
-            <h1 className="text-5xl font-bold">Register Here!</h1>
+            <h1 className="text-5xl font-bold italic">Register <span className='text-sky-500'>Here!</span> </h1>
          
           </div>
           <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
             <form className="card-body">
               <div className="form-control">
                 <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input type="text" placeholder="Name" className="input input-bordered" required name='name'/>
+              </div>
+
+
+              <div className="form-control">
+                <label className="label">
                   <span className="label-text">Email</span>
                 </label>
-                <input type="email" placeholder="email" className="input input-bordered" required />
+                <input type="email" placeholder="email" className="input input-bordered" required name='email' />
               </div>
+
+
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Password</span>
                 </label>
-                <input type="password" placeholder="password" className="input input-bordered" required />
-                <label className="label">
-                  <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                </label>
+                <input type="password" placeholder="password" className="input input-bordered" required name='password'/>
               </div>
+
+
               <div className="form-control mt-6">
                 <button className="btn btn-primary">Login</button>
               </div>
+
+
             </form>
             <p className='ml-4'>First time here? <Link to='/login' className='text-blue-600 font-bold'>Login here</Link> </p>
           </div>
